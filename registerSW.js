fb4f4b9bd87c947e2ec6c7bbe5f761b77afe7775
balamurugan-7git <palanivelbala94@gmail.com>
2026-01-31 23:10:58 +0530

    Deploy Unified Live Feed - Build Fix

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/zoro-expense-tracker-project1-/sw.js', { scope: '/zoro-expense-tracker-project1-/' })})}
