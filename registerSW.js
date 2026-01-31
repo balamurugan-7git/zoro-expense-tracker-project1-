4380bb2c2521a384066067a365553099954e795c
balamurugan-7git <palanivelbala94@gmail.com>
2026-02-01 00:51:09 +0530

    Final SEO & Gold Version Update

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/zoro-expense-tracker-project1-/sw.js', { scope: '/zoro-expense-tracker-project1-/' })})}
